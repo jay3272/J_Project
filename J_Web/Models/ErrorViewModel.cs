8e220b9a21f89e4fc7175a5c396e09ad53be4f0f
jay3272 <jaywu3272@gmail.com>
2023-03-31 14:38:12 +0800

    加入專案檔案。

---
+++ J_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace J_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
